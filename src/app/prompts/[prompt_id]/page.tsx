9572d78d0d5ad3c76521c7e04c70afa3a996191a
Rhaqim <anusiemj@gmail.com>
2024-04-20 17:25:55 +0100

    adding video

--- src/app/prompts/[prompt_id]/page.tsx
+++ src/app/prompts/[prompt_id]/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import React, { use, useEffect, useState } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { routes } from "@/service/api/routes";
+import { BASE_URL } from "@/config";
 
 type ReturnData = {
 	id: string;
@@ -15,6 +16,10 @@ type ReturnData = {
 
 export default function Page({ params }: { params: { prompt_id: string } }) {
 	const [prompt, setPrompt] = useState<ReturnData | null>(null);
+	const [videoBlob, setVideoBlob] = useState<Blob | null>(null);
+
+	const videoRef = useRef<HTMLVideoElement>(null);
+	const imageRef = useRef<HTMLImageElement>(null);
 
 	const promptResponse = async (choice: number, id: string) => {
 		const response = await routes.respond(choice, id);
@@ -32,6 +37,38 @@ export default function Page({ params }: { params: { prompt_id: string } }) {
 		fetchPrompt(params.prompt_id);
 	}, [params.prompt_id]);
 
+	useEffect(() => {
+		if (prompt && prompt) {
+			const fetchVideo = async (id: string) => {
+				try {
+					// use fetch POST request passing the id as story_id
+					const response = await fetch(`${BASE_URL}/video`, {
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify({ story_id: id }),
+					});
+
+					const blob = await response.blob();
+
+					setVideoBlob(blob);
+				} catch (error) {
+					console.error(error);
+				}
+			};
+			fetchVideo(prompt.id);
+		}
+	}, [prompt]);
+
+	useEffect(() => {
+		if (videoRef.current) {
+			// play the video and loop it
+			videoRef.current.play().catch((error) => console.error(error));
+			videoRef.current.loop = true;
+		}
+	}, [videoRef]);
+
 
 	if (!prompt) {
 		return <div>Loading...</div>;
@@ -43,6 +80,12 @@ export default function Page({ params }: { params: { prompt_id: string } }) {
 				<h1 className="text-3xl font-bold mb-4">Prompt Page</h1>
 				<div className="max-w-lg mx-auto bg-cover bg-center rounded-lg overflow-hidden shadow-lg">
 					<div className="p-8">
+						{videoBlob && (
+							<video ref={videoRef} className="w-full" controls>
+								<source src={videoBlob ? URL.createObjectURL(videoBlob) : ""} />
+								Your browser does not support the video tag.
+							</video>
+						)}
 						<h2 className="text-xl text-white mb-4 border border-gray-400 rounded-md p-4">
 							{prompt.story}
 						</h2>
