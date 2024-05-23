166f09b7b4f35780e972909ebb44e2076da61ea4
Rhaqim <anusiemj@gmail.com>
2024-05-23 22:45:40 +0100

    updating the config for nginx and pod

--- scripts/pod.sh
+++ scripts/pod.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Variables
-APP_NAME=my-nextjs-app
-CONTAINER_NAME=my-nextjs-app-container
-POD_NAME=my-nextjs-app-pod
+APP_NAME=playground
+CONTAINER_NAME=playground-container
+POD_NAME=playground-pod
 
 # Ensure the scripts directory is the working directory
 cd "$(dirname "$0")"
@@ -11,7 +11,8 @@ cd "$(dirname "$0")"
 # Create .env file if it doesn't exist
 if [ ! -f ../.env ]; then
   cat <<EOF > ../.env
-NEXT_PUBLIC_API_URL=http://localhost:8080
+NEXT_PUBLIC_BACKEND_DEV=http://localhost:8001
+NEXT_PUBLIC_BACKEND_PROD=http://localhost:8000
 EOF
 fi
 
@@ -24,11 +25,11 @@ podman rm -f $CONTAINER_NAME
 # Build the Docker image
 podman build -t $APP_NAME ..
 
-# Remove the existing pod if it exists
-podman pod rm -f $POD_NAME
+# # Remove the existing pod if it exists
+# podman pod rm -f $POD_NAME
 
-# Create a new pod
-podman pod create --name $POD_NAME -p 3000:3000 -p 8001:8001
+# # Create a new pod
+# podman pod create --name $POD_NAME -p 3000:3000 -p 8001:8001 -p 8000:8000
 
-# Run the container in the pod
-podman run -d --name $CONTAINER_NAME --pod $POD_NAME $APP_NAME
+# # Run the container in the pod
+# podman run -d --name $CONTAINER_NAME --pod $POD_NAME $APP_NAME
