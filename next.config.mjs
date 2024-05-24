853eb5d23c0ce364b2bf01237bdcdb3466863653
Rhaqim <anusiemj@gmail.com>
2024-05-24 14:51:46 +0100

    routes

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // siteUrl: "https://playground.degenerousdao.com",
     // reactStrictMode: true,
-    // images: {
-    //     domains: ["playground.degenerousdao.com"],
-    // },
     // async headers() {
     //     return [
     //         {
--- src/service/api/routes.ts
+++ src/service/api/routes.ts
@@ -4,12 +4,19 @@ import { apiFunctions, requestBlob } from "./provider";
 const { get, post, patch, del } = apiFunctions;
 
 export const routes = {
+	// Auth
+	login: async (wallet: string, signature: string) =>
+		post("/login", { wallet, signature }),
+	logout: async () => post("/logout", {}),
+	getNonce: async (wallet: string) => post("/nonce", { wallet }),
+	// Fetch
 	getPrompt: async () => get("/prompts"),
 	getTopics: async () => get("/topics"),
 	getCategories: async () => get("/categories"),
 	updateTopicCategory: async (topic_id: number, category_id: number) =>
 		patch(`/topic/${topic_id}`, { category_id }),
 	createCategory: async (name: string) => post("/category", { name }),
+	// Prompts
 	generatePrompt: async (data: TestPromptRequest) => post("/gen-prompt", data),
 	createPrompt: ({ prompt, category }: { prompt: string; category: number }) =>
 		post("/prompt", { prompt, category: category.toString() }),
@@ -27,11 +34,7 @@ export const routes = {
 		post("/start-prompt", { prompt_id: prompt_id }),
 	respond: async (choice: number, story_id: string) =>
 		post("/respond", { choice, story_id }),
-
-	login: async (wallet: string, signature: string) =>
-		post("/login", { wallet, signature }),
-	logout: async () => post("/logout", {}),
-	getNonce: async (wallet: string) => post("/nonce", { wallet }),
+	// Media
 	image: async (story_id: string) => post("/image", { story_id }),
 	video: async (story_id: string) =>
 		requestBlob("post", "/video", { story_id }),
