2799a85a1e09eef2eddecbcfd6c593e00674cf33
Rhaqim <anusiemj@gmail.com>
2024-07-20 01:26:28 +0100

    upload dir

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,7 +5,9 @@ export const BASE_URL_DEV =
 export const BASE_URL_PROD =
 	process.env.NEXT_PUBLIC_BACKEND_PROD || `http://localhost:8080`;
 
-export const UPLOAD_DIR = process.env.NEXT_UPLOAD_DIR || path.resolve(`./public`);
+export const UPLOAD_DIR = process.env.NEXT_UPLOAD_DIR || path.resolve(process.cwd());
+
+console.log("UPLOAD_DIR", UPLOAD_DIR);
 export const IMAGE_UPLOAD_DIR = path.resolve(UPLOAD_DIR, "images");
 export const MUSIC_UPLOAD_DIR = path.resolve(UPLOAD_DIR, "music");
 
