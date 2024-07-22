07cd9d09279bf7c716904687a6c1c85b9b887117
Rhaqim <anusiemj@gmail.com>
2024-07-22 02:18:44 +0100

    trying <img

--- src/components/File/ImageGallery.tsx
+++ src/components/File/ImageGallery.tsx
@@ -1,65 +1,74 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
-import Image from 'next/image';
+import { useEffect, useState } from "react";
+import Image from "next/image";
 
-import { useToast } from '@/context/toast.context';
+import { useToast } from "@/context/toast.context";
 
 const ImageGallery = () => {
-  const {addToast} = useToast();
+	const { addToast } = useToast();
 
-  const [images, setImages] = useState<string[]>([]);
+	const [images, setImages] = useState<string[]>([]);
 
-  useEffect(() => {
-    const fetchImages = async () => {
-      try {
-        const response = await fetch('/api/file/list?type=image');
-        if (response.ok) {
-          const data = await response.json();
-          setImages(data);
+	useEffect(() => {
+		const fetchImages = async () => {
+			try {
+				const response = await fetch("/api/file/list?type=image");
+				if (response.ok) {
+					const data = await response.json();
+					setImages(data);
 
-          addToast({
-            type: 'success',
-            message: 'Images fetched successfully',
-            id: ''
-          });
-        } else {
-          addToast({
-            type: 'error',
-            message: 'Error fetching images',
-            id: ''
-          });
-        }
-      } catch (error) {
-        addToast({
-          type: 'error',
-          message: `Error fetching images: ${error}`,
-          id: ''
-        });
-      }
-    };
+					addToast({
+						type: "success",
+						message: "Images fetched successfully",
+						id: "",
+					});
+				} else {
+					addToast({
+						type: "error",
+						message: "Error fetching images",
+						id: "",
+					});
+				}
+			} catch (error) {
+				addToast({
+					type: "error",
+					message: `Error fetching images: ${error}`,
+					id: "",
+				});
+			}
+		};
 
-    fetchImages();
-  }, []);
+		fetchImages();
+	}, []);
 
-  return (
-    <div>
-      <h1>Image Gallery</h1>
-      <div>
-        {images.map((image) => (
-          <Image
-            key={image}
-            src={image}
-            alt={image}
-            width={200}
-            height={200}
-            style={{ width: '100px', height: '100px', margin: '10px' }}
-            priority
-          />
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Image Gallery</h1>
+			<div>
+				{images.map(image => (
+					<>
+						<Image
+							key={image}
+							src={image}
+							alt={image}
+							width={200}
+							height={200}
+							style={{ width: "100px", height: "100px", margin: "10px" }}
+							priority
+						/>
+						<img
+							src={image}
+							alt={image}
+							width={200}
+							height={200}
+							style={{ width: "100px", height: "100px", margin: "10px" }}
+						/>
+					</>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default ImageGallery;
